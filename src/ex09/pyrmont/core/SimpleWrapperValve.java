d53e3cc9e2e19dbdb368f3289fa6dc796548dfe8
weigs <weiguangshuai@richstonedt.com>
2021-03-17 22:26:42 +0800

    学习笔记

---
+++ src/ex09/pyrmont/core/SimpleWrapperValve.java
@@ -0,0 +1,62 @@
+/**
+ * copied from ex08.pyrmont.core.SimpleWrapperValve
+ */
+package ex09.pyrmont.core;
+
+import org.apache.catalina.*;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SimpleWrapperValve implements Valve, Contained {
+
+    protected Container container;
+
+    public void invoke(Request request, Response response, ValveContext valveContext)
+            throws IOException, ServletException {
+
+        SimpleWrapper wrapper = (SimpleWrapper) getContainer();
+        ServletRequest sreq = request.getRequest();
+        ServletResponse sres = response.getResponse();
+        Servlet servlet = null;
+        HttpServletRequest hreq = null;
+        if (sreq instanceof HttpServletRequest)
+            hreq = (HttpServletRequest) sreq;
+        HttpServletResponse hres = null;
+        if (sres instanceof HttpServletResponse)
+            hres = (HttpServletResponse) sres;
+
+        //-- new addition -----------------------------------
+        Context context = (Context) wrapper.getParent();
+        request.setContext(context);
+        //-------------------------------------
+        // Allocate a servlet instance to process this request
+        try {
+            servlet = wrapper.allocate();
+            if (hres != null && hreq != null) {
+                servlet.service(hreq, hres);
+            } else {
+                servlet.service(sreq, sres);
+            }
+        } catch (ServletException e) {
+        }
+    }
+
+    public String getInfo() {
+        return null;
+    }
+
+    public Container getContainer() {
+        return container;
+    }
+
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+}
